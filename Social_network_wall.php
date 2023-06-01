103da3368342d71617ed6a5dc5a6fe9c86eb2735
Boran Deniz Düzgün <dante_deniz@hotmail.com>
2023-06-01 14:29:38 +0300

    Update Social_network_wall.php

--- Social_network_wall.php
+++ Social_network_wall.php
@@ -644,13 +644,13 @@
                                     <div class="input-group mda-input-group">
                                         <div class="mda-form-group">
                                             <div class="mda-form-control">
-                                                <textarea rows="1" aria-multiline="true" tabindex="0" aria-invalid="false" class="no-resize form-control" name="txt" value="<?= isset($txt) ? filter_var($txt, FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "1" ?>"></textarea>
+                                                <textarea rows="1" aria-multiline="true" tabindex="0" aria-invalid="false" class="no-resize form-control" name="txt" value="<?= isset($txt) ? filter_var($txt, FILTER_SANITIZE_FULL_SPECIAL_CHARS) : "1" ?>" id="share_post_txt"></textarea>
                                                 <div class="mda-form-control-line"></div>
                                                 <label class="m0">What's on your mind?</label>
                                             </div><span class="mda-form-msg right">Any message here</span>
                                         </div>
                                         <span class="input-group-btn">
-                                            <button type="button" class="btn btn-success" style="margin-top:-44px">
+                                            <button type="button" class="btn btn-success" style="margin-top:-44px" id="share_post">
                                                 POST
                                             </button>
                                         </span>
@@ -806,6 +806,11 @@
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 
     <script type="text/javascript">
+        $i=3;
+        $(`#share_post`).on(`click`, function(){
+            $e = $("<div class='card-body'><div class='card'><div class='card-heading'><div class='media m0'><div class='media-left'><a href='#'><img src='https://bootdey.com/img/Content/avatar/avatar6.png' alt='User' class='media-object img-circle thumb48'></a></div><div class='media-body media-middle pt-sm'><p class='media-heading m0 text-bold'>Stephen Palmer</p><small class='text-muted'><em class='ion-earth text-muted mr-sm'></em><span>2 hours</span></small></div></div><div class='p'></div></div><div class='card-footer'><button type='button' class='btn btn-flat btn-primary like' id='like1'>Like</button><button type='button' class='btn btn-flat btn-primary comment' id='comment1'>Comment</button><div class='comment_to_post' id='comment1_to_post'><textarea rows='1' aria-multiline='true' tabindex='0' aria-invalid='false' class='no-resize form-control' name='txt'></textarea></div><div class='comment_to_post' id='comment1_by_user'></div></div></div></div>");
+            $(`.posts`).prepend($e);
+        })
 
         $i=1;
         $(`#notifications`).on(`click`, function(){
@@ -853,7 +858,7 @@
                 $e = $("<div class='inside'><p></p></div>");
                 $e.attr(`id`, `${$t}_by_user${$i}`);
 
-                $(`#${$t}_by_user`).append($e);
+                $(`#${$t}_by_user`).prepend($e);
                 $(`#${$t}_by_user${$i}`).text(`${$val}`);
                 
             }
