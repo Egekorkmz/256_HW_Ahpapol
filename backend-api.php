3ce8e8a29e29966fde16076fed12078f72982107
Kitsune1890 <zyildiz966@gmail.com>
2023-06-02 16:52:06 +0300

    no sleep

--- backend-api.php
+++ backend-api.php
@@ -8,53 +8,43 @@
   $input = json_decode($json) ; 
 
   if ( $method === "GETPOST") {
-     $out = getPosts($input->user_id) ;
-     sleep(1) ;  
+     $out = getPosts($input->user_id) ; 
   }
 
   if ( $method === "GETFRIENDPOST") {
-    $out = getFriendsPosts($input->user_id, $input->limit);
-    sleep(1) ;  
+    $out = getFriendsPosts($input->user_id, $input->limit); 
  }
 
   if( $method === "GETCOMMENT"){
     $out = getComments($input->post_id);
-    sleep(1) ;
   }
 
   if ( $method === "FRIEND") {
     $out = getFriends($input->user_id);
-    sleep(1) ;  
  }
 
   if ( $method === "GETNOT") {
       $out = getNotifications($input->user_id);
-      sleep(1) ;
   }
 
   if ( $method === "POST") {
       $out = addPost($input->user_id, $input->text);
-      sleep(1) ;
   }
 
   if ( $method === "POSTCOMMENT") {
       $out = addComment($input->user_id, $input->post_id, $input->text);
-      sleep(1) ;
   }
 
   if( $method === "POSTFRIEND"){
     $out = addFriend($input->user_id, $input->friend_id);
-    sleep(1) ;
   }
 
   if( $method === "POSTREQUEST"){
     $out = friendRequest($input->reciever, $input->sender, $input->type);
-    sleep(1) ;
   }
 
   if( $method === "FINDUSER"){
     $out = findUser($input->filter, $input->keyword);
-    sleep(1) ;
   }
 
   echo json_encode($out) ;
